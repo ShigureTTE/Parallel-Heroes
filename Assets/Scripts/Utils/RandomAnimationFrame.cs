ee7dc15d6e2cd90766f2f8ca8bd473a2fff59328
Shi <g.vandenbiggelaar@gmail.com>
2019-11-18 10:00:04 +0100

    close range positions

---
+++ Assets/Scripts/Utils/RandomAnimationFrame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAnimationFrame : MonoBehaviour {
+
+	void OnEnable () {
+        if (GetComponent<Animator>())
+        {
+            Animator anim = GetComponent<Animator>();
+            AnimatorStateInfo state = anim.GetCurrentAnimatorStateInfo(0);
+            anim.Play(state.fullPathHash, -1, Random.Range(0f, 1f));
+        }
+    }
+
+}
