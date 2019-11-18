5eae2e805c4ee04a6507be581b7e9d63883dc6ed
Shi <g.vandenbiggelaar@gmail.com>
2019-11-18 15:13:02 +0100

    base for UI

--- Assets/Controls.cs
+++ Assets/Controls.cs
@@ -19,9 +19,17 @@ public class @Controls : IInputActionCollection, IDisposable
             ""id"": ""1e20ed67-9e2c-4322-b4ce-8dd8c48c7ccf"",
             ""actions"": [
                 {
-                    ""name"": ""PressButton]"",
+                    ""name"": ""PressButton"",
                     ""type"": ""Button"",
                     ""id"": ""6242d91a-05ed-474f-a80c-1f7f1c5eaf23"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Navigate"",
+                    ""type"": ""Button"",
+                    ""id"": ""6c497da3-925a-4321-a8da-a1d6ed65c44c"",
                     ""expectedControlType"": """",
                     ""processors"": """",
                     ""interactions"": """"
@@ -35,9 +43,64 @@ public class @Controls : IInputActionCollection, IDisposable
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": ""Player"",
-                    ""action"": ""PressButton]"",
+                    ""action"": ""PressButton"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""ArrowKeys"",
+                    ""id"": ""bfa20cca-3a13-41cb-8f81-2b6a4a5e3e4d"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Navigate"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""c0928ef2-3f69-413c-bdc1-80bdc468197f"",
+                    ""path"": ""<Keyboard>/upArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Player"",
+                    ""action"": ""Navigate"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""b80b9147-3ccf-49c5-8383-bec5b6f4114c"",
+                    ""path"": ""<Keyboard>/downArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Player"",
+                    ""action"": ""Navigate"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""562ebc46-dd88-49d1-aa75-879ec3806d21"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Player"",
+                    ""action"": ""Navigate"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""990e2465-2728-47e8-a53c-d1e485d284f9"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Player"",
+                    ""action"": ""Navigate"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
                 }
             ]
         }
@@ -63,7 +126,8 @@ public class @Controls : IInputActionCollection, IDisposable
 }");
         // Debug
         m_Debug = asset.FindActionMap("Debug", throwIfNotFound: true);
-        m_Debug_PressButton = m_Debug.FindAction("PressButton]", throwIfNotFound: true);
+        m_Debug_PressButton = m_Debug.FindAction("PressButton", throwIfNotFound: true);
+        m_Debug_Navigate = m_Debug.FindAction("Navigate", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -114,11 +178,13 @@ public class @Controls : IInputActionCollection, IDisposable
     private readonly InputActionMap m_Debug;
     private IDebugActions m_DebugActionsCallbackInterface;
     private readonly InputAction m_Debug_PressButton;
+    private readonly InputAction m_Debug_Navigate;
     public struct DebugActions
     {
         private @Controls m_Wrapper;
         public DebugActions(@Controls wrapper) { m_Wrapper = wrapper; }
         public InputAction @PressButton => m_Wrapper.m_Debug_PressButton;
+        public InputAction @Navigate => m_Wrapper.m_Debug_Navigate;
         public InputActionMap Get() { return m_Wrapper.m_Debug; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -131,6 +197,9 @@ public class @Controls : IInputActionCollection, IDisposable
                 @PressButton.started -= m_Wrapper.m_DebugActionsCallbackInterface.OnPressButton;
                 @PressButton.performed -= m_Wrapper.m_DebugActionsCallbackInterface.OnPressButton;
                 @PressButton.canceled -= m_Wrapper.m_DebugActionsCallbackInterface.OnPressButton;
+                @Navigate.started -= m_Wrapper.m_DebugActionsCallbackInterface.OnNavigate;
+                @Navigate.performed -= m_Wrapper.m_DebugActionsCallbackInterface.OnNavigate;
+                @Navigate.canceled -= m_Wrapper.m_DebugActionsCallbackInterface.OnNavigate;
             }
             m_Wrapper.m_DebugActionsCallbackInterface = instance;
             if (instance != null)
@@ -138,6 +207,9 @@ public class @Controls : IInputActionCollection, IDisposable
                 @PressButton.started += instance.OnPressButton;
                 @PressButton.performed += instance.OnPressButton;
                 @PressButton.canceled += instance.OnPressButton;
+                @Navigate.started += instance.OnNavigate;
+                @Navigate.performed += instance.OnNavigate;
+                @Navigate.canceled += instance.OnNavigate;
             }
         }
     }
@@ -154,5 +226,6 @@ public class @Controls : IInputActionCollection, IDisposable
     public interface IDebugActions
     {
         void OnPressButton(InputAction.CallbackContext context);
+        void OnNavigate(InputAction.CallbackContext context);
     }
 }
