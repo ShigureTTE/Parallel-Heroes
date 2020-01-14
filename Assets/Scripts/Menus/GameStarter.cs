bf04c4e25ca633f66b105636bcd2eff5f8c34de8
Shi <g.vandenbiggelaar@gmail.com>
2020-01-14 15:39:51 +0100

    blue palette swap and title screen

---
+++ Assets/Scripts/Menus/GameStarter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStarter : MonoBehaviour {
+
+    [SerializeField] private RandomCharacter randomCharacter;
+    [SerializeField] private ScreenFade fade;
+    [SerializeField] private Area areaObject;
+    [SerializeField] private string sceneTransitionName;
+
+    public void StartGame() {
+        randomCharacter.SpawnCharacter();
+        Game.Instance.PlayerParty.ResetCharacters();
+        Game.Instance.CurrentArea = areaObject;
+
+        fade.FadeOut(() => Game.Instance.ChangeScene(sceneTransitionName, UnityEngine.SceneManagement.LoadSceneMode.Single));
+    }
+}
